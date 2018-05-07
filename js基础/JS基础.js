f20be422a5d823f9308881e7426520498ea015b4
kraven <Kraven_L@163.com>
2018-05-07 16:47:55 +0800

    绑定input与label

--- js基础/JS基础.js
+++ js基础/JS基础.js
@@ -1,7 +1,11 @@
-    // string.indexof()
+    // string.indexOf()
     var str = "ybasidyufibdfcasdf";
 
     console.log(str.indexOf("b")); // 1
     
     // 判断 括号中字符串在string字符串中第一次出现的索引位置 
     // 没有就返回 -1
+
+    // 把 input和label绑定起来的方法
+    // <input type="checkbox" id="XXXX"></input>
+    // <label for="XXXX"></label>
